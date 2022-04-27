6e0035bc6cb109eb703a2c1edd6e7b9fa4d516cb
NikoLuka <311509080311@home.hpu.edu.cn>
2022-04-27 14:31:46 +0800

    cs

--- SpringBootTest/src/main/java/com/example/springboottest/SpringBootTestApplication.java
+++ SpringBootTest/src/main/java/com/example/springboottest/SpringBootTestApplication.java
@@ -3,7 +3,7 @@ package com.example.springboottest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//
 public class SpringBootTestApplication {
 
     public static void main(String[] args) {
